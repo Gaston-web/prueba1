0c35b3de1bbfcb13865de31ae914ae47621cea05
Gaston Ghilardo <gastonghilardo@hotmail.com>
2025-09-29 23:51:31 -0300

    agrego Routers, Nav

---
+++ src/componentes/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const navList = [
+    {nombre:'Home',
+        to:'/'
+    },
+    {nombre:'Login',
+        to:'/Login'
+    },
+]
+
+export const Nav = () => {
+  return (
+    <div className='m-0 p-2 bg-black flex justify-center gap-3'>
+        {navList.map((i)=><Link className='border-white bg-black border-2 hover:cursor-pointer ' to={i.to}>{i.nombre}</Link>)}
+    </div>
+  )
+}
